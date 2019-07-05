a11e0bceae7e0b00943081b9f0359e70b51672fc
Dario Kemenovic <dkemenovic@gmail.com>
2019-07-05 16:18:52 +0200

    Create input

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -1,2 +1,16 @@
+import React, { Component } from 'react';
 
+class AddPlayerForm extends Component {
+	render() {
+		return (
+			<form>
+				<input 
+					type="text"
+					placeholder="Enter a player's name"
+				/>
+			</form>
+		);
+	}
+}
 
+export default AddPlayerForm;
