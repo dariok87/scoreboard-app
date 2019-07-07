cf0aedd584e500051e5a54fe5ca4f32167761aa1
Dario Kemenovic <dkemenovic@gmail.com>
2019-07-07 19:27:37 +0200

    Add event and function

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -5,12 +5,20 @@ class Stopwatch extends Component {
 		isRunning: false
 	};
 
+	handleStopwatch = () => {
+		this.setState({
+			isRunning: !this.state.isRunning
+		});
+	}
+
   render() {
 		return (
 			<div className="stopwatch">
 				<h2>Stopwatch</h2>
 				<span className="stopwatch-time">0</span>
-				<button>Start</button>
+				<button onClick={this.handleStopwatch}>
+					{ this.state.isRunning ? 'Stop' : 'Start' }
+				</button>
 				<button>Reset</button>
 			</div>
 		);
