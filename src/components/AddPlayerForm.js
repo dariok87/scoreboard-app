d6043264e1b79d514b7e6dcce39c9846c9f62f4a
Dario Kemenovic <dkemenovic@gmail.com>
2019-07-10 19:14:50 +0200

    Pass ref in the callback function

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -14,7 +14,7 @@ class AddPlayerForm extends Component {
 
 	handleSubmit = (e) => {
 		e.preventDefault();
-		this.props.addPlayer(this.playerInput);
+		this.props.addPlayer(this.playerInput.current.value);
 		this.setState({ value: ''});
 	}
 
