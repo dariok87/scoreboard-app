70a274e385b76b0bbe69fe23ebfc7a461e571614
Dario Kemenovic <dkemenovic@gmail.com>
2019-07-10 19:17:17 +0200

    Delete method

--- src/components/AddPlayerForm.js
+++ src/components/AddPlayerForm.js
@@ -7,7 +7,6 @@ class AddPlayerForm extends Component {
 	handleSubmit = (e) => {
 		e.preventDefault();
 		this.props.addPlayer(this.playerInput.current.value);
-		this.setState({ value: ''});
 	}
 
 	render() {
