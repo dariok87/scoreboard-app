63fa6824033ff5cdfc7bd9b1d63d250e44b9ec8c
Dario Kemenovic <dkemenovic@gmail.com>
2019-07-12 20:59:55 +0200

    Delete import of Icon component

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import Header from './Header';
 import Player from './Player';
 import AddPlayerForm from './AddPlayerForm';
-import Icon from './Icon';
 
 class App extends Component {
   state = {
