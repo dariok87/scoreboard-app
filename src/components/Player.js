1c696655d408f0da60c2be5a9a3bbf4f5d3fc605
Dario Kemenovic <dkemenovic@gmail.com>
2019-07-12 21:39:51 +0200

    Add comment

--- src/components/Player.js
+++ src/components/Player.js
@@ -30,7 +30,7 @@ class Player extends PureComponent{
 				<span className="player-name">
 					<button className="remove-player" onClick={() => removePlayer(id)}>✖</button>
 					
-					<Icon isHighScore={this.props.isHighScore}/> // crown icon
+					<Icon isHighScore={this.props.isHighScore}/> {/* crown icon */}
 					{ name }
 				</span>
 	
